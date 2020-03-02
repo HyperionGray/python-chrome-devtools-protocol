f4525a7746704f5ec2dae16ae2dff66f57699324
Mark E. Haase <mehaase@gmail.com>
2020-03-02 12:39:13 -0500

    Fix broken tests

--- generator/test_generate.py
+++ generator/test_generate.py
@@ -928,19 +928,41 @@ def test_cdp_domain_sphinx():
         Types
         -----
 
-        Generally you do not need to instantiate CDP types yourself. Instead, the API creates objects for you as return values from commands, and then you can use those objects as arguments to other commands.
+        Generally, you do not need to instantiate CDP types
+        yourself. Instead, the API creates objects for you as return
+        values from commands, and then you can use those objects as
+        arguments to other commands.
 
         .. autoclass:: KeyframeStyle
+              :members:
+              :undoc-members:
+              :exclude-members: from_json, to_json
 
         Commands
         --------
+
+        Each command is a generator function. The return
+        type ``Generator[x, y, z]`` indicates that the generator
+        *yields* arguments of type ``x``, it must be resumed with
+        an argument of type ``y``, and it returns type ``z``. In
+        this library, types ``x`` and ``y`` are the same for all
+        commands, and ``z`` is the return type you should pay attention
+        to. For more information, see
+        :ref:`Getting Started: Commands <getting-started-commands>`.
 
         .. autofunction:: get_current_time
 
         Events
         ------
 
+        Generally, you do not need to instantiate CDP events
+        yourself. Instead, the API creates events for you and then
+        you use the event's attributes.
+
         .. autoclass:: AnimationCanceled
+              :members:
+              :undoc-members:
+              :exclude-members: from_json, to_json
     """)
     domain = CdpDomain.from_json(json_domain)
     actual = domain.generate_sphinx()
